c8f9f38f6e8fc8cdcd7e7665556bf1264ddf3dbc
blazejmanczak <manczakb@gmail.com>
2021-12-13 14:49:43 +0100

    yaml preprocessing func

---
+++ experiments/preprocess_config.py
@@ -0,0 +1,70 @@
+from numpy import save
+import yaml
+from grid2op_env.grid_to_gym import Grid_Gym
+from callback import LogDistributionsCallback
+from ray import tune
+
+mapper = {
+    "callbacks":{
+        "LogDistributionsCallback": LogDistributionsCallback
+    },
+    "env":{
+        "Grid_Gym": Grid_Gym
+    }
+}
+def preprocess_config(config):
+    """
+    Transform the string representations of classes in YAML
+    files to the corresponding python objects.
+
+    Args:
+        config (dict): parsed YAML config file
+    """
+    if "callbacks" in config["tune_config"]:
+        config["tune_config"]["callbacks"] = mapper["callbacks"][config["tune_config"]["callbacks"]]
+    try:
+        config["tune_config"]["env"] = mapper["env"][config["tune_config"]["env"]]
+    except: # if the env is not in the mapper, it is an already registerd env
+        pass
+
+
+    return config
+
+    
+def tune_search_quniform_constructor(loader, node):
+    """
+    Constructor for tune uniform float sampling  
+
+    """
+    vals = []
+    for scalar_node in node.value:
+        vals.append(float(scalar_node.value))
+    return tune.quniform(vals[0], vals[1], vals[2])
+
+def tune_search_grid_search_constructor(loader, node):
+    """
+    Constructor for tune grid search.
+
+    """
+    vals = []
+    for scalar_node in node.value:
+        vals.append(float(scalar_node.value))
+    return tune.grid_search(vals)
+
+def tune_choice_constructor(loader, node):
+    """
+    Constructor for tune grid search.
+
+    """
+    vals = []
+    for scalar_node in node.value:
+        vals.append(float(scalar_node.value))
+    return tune.choice(vals)
+    
+def get_loader():
+  """Add constructors to PyYAML loader."""
+  loader = yaml.SafeLoader
+  loader.add_constructor("!quniform", tune_search_quniform_constructor)
+  loader.add_constructor("!grid_search", tune_search_grid_search_constructor)
+  loader.add_constructor("!choice", tune_choice_constructor)
+  return loader
